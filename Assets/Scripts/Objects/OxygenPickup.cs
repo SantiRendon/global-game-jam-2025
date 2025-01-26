0596ce705c605ba8059c61c05b97270e71e5299a
santiDev <xantirendonc@gmail.com>
2025-01-25 21:13:24 -0500

    fix oxygenPickup

--- Assets/Scripts/Objects/OxygenPickup.cs
+++ Assets/Scripts/Objects/OxygenPickup.cs
@@ -7,11 +7,18 @@ public class OxygenPickup : MonoBehaviour
         // Verifica si el jugador tocó el objeto
         if (collision.CompareTag("Player"))
         {
-            // Accede al script OxygenManager desde el jugador
-            OxygenManager oxygenManager = collision.GetComponent<OxygenManager>();
+            // Accede al script OxygenManager desde un GameObject que contiene el script
+            OxygenManager oxygenManager = FindObjectOfType<OxygenManager>();
+
+            // Verifica si el OxygenManager está presente en la escena
             if (oxygenManager != null)
             {
                 oxygenManager.RestoreOxygen(); // Restaura el oxígeno al 100%
+                Debug.Log("Oxígeno restaurado.");
+            }
+            else
+            {
+                Debug.LogWarning("No se encontró OxygenManager en la escena.");
             }
 
             // Destruir el objeto después de ser recogido
